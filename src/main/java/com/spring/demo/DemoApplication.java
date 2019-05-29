709359b08aeb8019bf86c9e81b27c98188b73219
Peng <visualrainy@gmail.com>
2019-05-29 15:49:30 +0800

    Add ActiveMQ for test demo

--- src/main/java/com/spring/demo/DemoApplication.java
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -1,14 +1,17 @@
 package com.spring.demo;
 
 import com.spring.demo.config.AuthorConfig;
+import com.spring.demo.msg.Msg;
 import com.spring.demo.support.CustomRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.Banner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @EnableJpaRepositories(repositoryFactoryBeanClass = CustomRepositoryFactoryBean.class)
 @EnableCaching
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
 
     @Autowired
     private AuthorConfig authorConfig;
@@ -39,4 +45,9 @@ public class DemoApplication {
         new SpringApplicationBuilder(DemoApplication.class).bannerMode(Banner.Mode.OFF).run(args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        //定义程序启动后执行的代码
+        jmsTemplate.send("my-destination", new Msg());
+    }
 }
