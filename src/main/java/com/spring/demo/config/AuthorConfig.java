0b084413ef2be4f0b8648ba425954a18d8fb5e32
Peng <visualrainy@gmail.com>
2019-07-15 21:19:06 +0800

    Use lombok instead of getter and setter

--- src/main/java/com/spring/demo/config/AuthorConfig.java
+++ src/main/java/com/spring/demo/config/AuthorConfig.java
@@ -19,20 +19,4 @@ public class AuthorConfig {
     @Getter
     @Setter
     private String age;
-
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getAge() {
-//        return age;
-//    }
-//
-//    public void setAge(String age) {
-//        this.age = age;
-//    }
 }
